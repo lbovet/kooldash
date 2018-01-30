7445397746fc559768f89e9c2111f78790019b6f
Laurent Bovet <laurent.bovet@windmaster.ch>
2018-01-31 00:33:31 +0100

    Finished weather

--- weather/weather.js
+++ weather/weather.js
@@ -1,42 +1,50 @@
 var report$ = rx.timer(0, 60000)
   .flatMap(_ => rx.ajax(config.weather.url)
-  .map(data => data.response))
-  .share();
+    .map(data => data.response))
+    .share();
 
-var forecasts$ = report$
-  .flatMap(report => [ report.fcst_day_0, report.fcst_day_1 ])
-  .flatMap(day => rx.pairs(day.hourly_data)
-    .map(pair => [ pair[0].length == 5 ? pair[0]: "0"+pair[0], pair[1]])
-    .toArray()
-    .flatMap(arr => arr.sort((l,r) => l[0].localeCompare(r[0])))
-  )
-  .filter(pair => pair[0] == "10H00" || pair[0] == "14H00" || pair[0] == "18H00")
-  .map(pair => pair[1])
-  .toArray()
+var current$ = () => report$
+  .map(report => Object({
+    icon: report.current_condition.condition_key,
+    temp: report.current_condition.tmp,
+    tempMax: report.fcst_day_0.tmax,
+    tempMin: report.fcst_day_0.tmin
+  }));
+
+var forecasts$ = () => report$
+  .flatMap(report => rx.from([ report.fcst_day_0, report.fcst_day_1 ])
+    .flatMap(day => rx.pairs(day.hourly_data)
+      .map(pair => [ pair[0].length == 5 ? pair[0]: "0"+pair[0], pair[1]])
+      .toArray()
+      .flatMap(arr => arr.sort((l,r) => l[0].localeCompare(r[0])))
+    )
+    .filter(pair => pair[0] == "10H00" || pair[0] == "14H00" || pair[0] == "18H00")
+    .map(pair => Object({
+      time: pair[0].replace("H", ":"),
+      icon: pair[1].CONDITION_KEY,
+      temp: Math.round(pair[1].TMP2m)
+    }))
+    .do(console.log)
+    .toArray())
 
 Vue.component('weather', function(resolve) {
   rx.ajax({ url: "weather/weather.html", responseType: "text"})
   .map(data => Object({
     template: data.response,
     subscriptions: {
-      currentIcon: report$.map(report => report.current_condition.condition_key),
-      currentTemp: report$.map(report => report.current_condition.tmp),
-      maxTemp: report$.map(report => report.fcst_day_0.tmax),
-      minTemp: report$.map(report => report.fcst_day_0.tmin),
-      forecasts: forecasts$
+      current: current$(),
+      forecasts: forecasts$()
     },
     methods: {
       iconClass: function(icon, size) {
-        if(icon){
-          var result = {
-            wu: true,
-            "wu-white": true,
-            "wu-night": (icon.indexOf("nuit") != -1)
-          }
-          result["wu-"+size] = true;
-          result["wu-"+icons[icon]] = true;
-          return result;
+        var result = {
+          wu: true,
+          "wu-white": true,
+          "wu-night": (icon.indexOf("nuit") != -1)
         }
+        result["wu-"+size] = true;
+        result["wu-"+icons[icon]] = true;
+        return result;
       }
     }
   }))
