bb9f5628a62883c59c9ace948462dad679ef2ad8
Laurent Bovet <laurent.bovet@windmaster.ch>
2018-02-03 01:22:32 +0100

    Added keys to command player

---
+++ index.js
@@ -0,0 +1,16 @@
+window.main = new Vue({ el: "#main",
+  created: function() {
+    console.log("r")
+      var vm = this;
+      window.addEventListener('keyup', function(event) {
+        switch(event.keyCode) {
+          case 38: vm.$emit('up'); break;
+          case 40: vm.$emit('down'); break;
+          case 32: vm.$emit('space'); break;
+          case 49: vm.$emit('1'); break;
+          case 50: vm.$emit('2'); break;
+          case 51: vm.$emit('3'); break;
+        }
+      });
+    }
+  })
--- player/player.js
+++ player/player.js
@@ -1,7 +1,10 @@
+var state = null;
+
 var status$ = () =>
   rx.timer(0, 2000)
-  .flatMap(_ => rx.ajax({url: config.player.url+"/media/status"})
-    .map(data => data.response))
+  .flatMap(_ => rx.ajax(config.player.url+"/media/status")
+    .map(data => data.response)
+    .do(status => state = status.player_state))
 
 Vue.component('player', function(resolve) {
   rx.ajax({ url: "player/player.html", responseType: "text"})
@@ -9,6 +12,27 @@ Vue.component('player', function(resolve) {
     template: data.response,
     subscriptions: {
       status: status$()
+    },
+    created: function() {
+      window.main.$on('up', function() {
+        rx.ajax({ url: config.player.url+"/control/up", method: "POST"}).subscribe();
+      });
+      window.main.$on('down', function() {
+        rx.ajax({ url: config.player.url+"/control/down", method: "POST"}).subscribe();
+      })
+      window.main.$on('1', function() {
+        rx.ajax({ url: config.player.url+"/control/1", method: "POST"}).subscribe();
+      })
+      window.main.$on('3', function() {
+        rx.ajax({ url: config.player.url+"/control/3", method: "POST"}).subscribe();
+      })
+      window.main.$on('space', function() {
+        if(state == "PLAYING") {
+          rx.ajax({ url: config.player.url+"/control/pause", method: "POST"}).subscribe();
+        } else {
+          rx.ajax({ url: config.player.url+"/control/play", method: "POST"}).subscribe();
+        }
+      })
     }
   }))
   .subscribe(resolve);
